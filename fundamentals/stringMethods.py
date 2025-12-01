5979543423b3a5ba4a05711ae05fe0def03358f7
Melrose Ceballos <198425854+cobraxcodes@users.noreply.github.com>
2025-11-30 19:01:25 -0800

    practice string methods

--- fundamentals/stringMethods.py
+++ fundamentals/stringMethods.py
@@ -13,6 +13,23 @@ print(d)
 f = a.center(20)
 print(f)
 
-#count
+#count -counts how many characters are in strings
 g = a.count("l")
 print(g)
+
+#encode - turns string into machine language
+h = a.encode()
+print(h)
+
+#endswith - checks if string ends with a certain character
+i = a.endswith("o")
+print(i)
+
+#expandtabs() - to create specific spacing w/ tabs
+j = "h\te\tl\tl\tl\to"
+k = j.expandtabs(20)
+print(k)
+
+#find - looks for specific character and returns the index of character
+l  = a.find("h")
+print(l)
