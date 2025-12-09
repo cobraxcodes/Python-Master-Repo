892998f3215f052ad3371f715826bea507441eaf
Melrose Ceballos <198425854+cobraxcodes@users.noreply.github.com>
2025-12-08 22:53:52 -0800

    Unpack, convert, and create tuples

--- fundamentals/tuples.py
+++ fundamentals/tuples.py
@@ -4,3 +4,18 @@ print(colors[1])
 
 for color in colors:
     print(color)
+
+# tuple unpacking
+person = ("Cobra", "Backend", "Python")
+name, role, skill = person
+print(skill)
+
+#nested tuple
+nest = ("hello", ["apples", "oranges", "pears"], ("green", "brown", "black"))
+print(nest[2][1])
+
+#converting to tuples
+nums = [1,2,3,4]
+nums_tuples = tuple(nums)
+print(nums_tuples)
+print(type(nums_tuples))
