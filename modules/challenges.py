6c4113d3d592bc4fa00cfe311dbca761d2002b4e
cobraxcodes <198425854+cobraxcodes@users.noreply.github.com>
2026-01-01 18:49:10 -0800

    create function that prompts user input and creates hexa or rgb values in x amounts dependent on user input

--- modules/challenges.py
+++ modules/challenges.py
@@ -1,7 +1,18 @@
-# #generate random username
+import sample
+# generate random username
 # from sample import random_user_id
 # print(random_user_id())
 
-#rename function and take two inputs (number of characters, numbers of IDs generated)
-from sample import random_user_id as input_user_ids
-input_user_ids()
+# #rename function and take two inputs (number of characters, numbers of IDs generated)
+# from sample import random_user_id as input_user_ids
+# input_user_ids()
+
+from sample import rgb_color_gen
+rgb_color_gen()
+
+from sample import list_of_hexa_colors
+list_of_hexa_colors(3)
+
+sample.list_of_rgb_colors(3)
+
+sample.generate_colors()
--- modules/sample.py
+++ modules/sample.py
@@ -26,3 +26,33 @@ def rgb_color_gen(): #generates rgb colors from range 0 to 255
     second_color = random.randint(0,255)
     third_color = random.randint(0,255)
     print (f"rgb({first_color}, {second_color}, {third_color})")
+
+
+## LEVEL 2
+def list_of_hexa_colors(n): # returns x amount of hexidecimal colors in an array
+    arr=[]
+    for _ in range(n):
+        arr.append('#' + ''.join(random.choices(string.ascii_letters[:6].lower() + string.digits, k=6)))
+    print(arr)
+
+def list_of_rgb_colors(n): # returns x amount of rgb colors in an array
+    arr=[]
+    for _ in range(n): 
+        arr.append(random.randint(0,255))
+    print(arr)
+
+def generate_colors(): # returns a hexa or rgb values in x amount dependent on user input
+    prompt = input("Would you like to generate a hexa or rgb?" + '')
+    n = int(input("How many colors would you like?"))
+    if prompt == 'hexa':
+        arr = []
+        for _ in range(n):
+         arr.append('#' + ''.join(random.choices(string.ascii_letters[:6].lower() + string.digits, k=6)))
+        print (arr)
+    elif prompt == 'rgb':
+        arr_rgb=[]
+        for _ in range(n):
+         arr_rgb.append(random.randint(0,255))
+        print(arr_rgb)
+    else:
+        print ("Invalid response")
