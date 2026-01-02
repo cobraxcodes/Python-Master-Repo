a28b018d9446d1dfadaab129957ad219cc6b1c13
cobraxcodes <198425854+cobraxcodes@users.noreply.github.com>
2026-01-01 16:32:29 -0800

    create user ids length and amount from user input

--- modules/challenges.py
+++ modules/challenges.py
@@ -1,3 +1,7 @@
-#generate random username
-from sample import random_user_id
-print(random_user_id())
+# #generate random username
+# from sample import random_user_id
+# print(random_user_id())
+
+#rename function and take two inputs (number of characters, numbers of IDs generated)
+from sample import random_user_id as input_user_ids
+input_user_ids()
